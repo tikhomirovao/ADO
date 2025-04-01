81fd6681afd3d5d1a59a5880dc9b0c344f0ed9eb
hawri <hawri@DESKTOP-BMM1386>
2025-04-01 11:35:35 +0300

    HARDCORE DONE

---
+++ Academy/Query.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+    internal class Query
+    {
+        public string Columns { get; }
+        public string Tables { get; }
+        public string Condition { get; }
+        public string Group_by { get; }
+        public Query(string columns, string tables, string condition = "", string group_by = "")
+        {
+            Columns = columns;
+            Tables = tables;
+            Condition = condition;
+            Group_by = group_by;
+        }
+    }
+}
